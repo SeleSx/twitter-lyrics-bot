7619b1029d066ffddef1919e67325dc4e84f9dc1
Saffet Sinanoğlu <saffet.sinanoglu@hotmail.com>
2024-05-30 18:49:37 +0300

    history works now

--- asd.py
+++
@@ -1,4 +0,0 @@
-import datetime
-
-now = datetime.datetime.now()
-print(type(now.minute))
--- tweet_shit.py
+++ tweet_shit.py
@@ -3,88 +3,125 @@ import os
 import json
 import datetime
 import time
+import random
+
+def authToTwitter():
+    # In your terminal please set your environment variables by running the following lines of code.
+    # export 'CONSUMER_KEY'='<your_consumer_key>'
+    # export 'CONSUMER_SECRET'='<your_consumer_secret>'
+
+    consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+    consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
 
 
-# In your terminal please set your environment variables by running the following lines of code.
-# export 'CONSUMER_KEY'='<your_consumer_key>'
-# export 'CONSUMER_SECRET'='<your_consumer_secret>'
+    # Be sure to add replace the text of the with the text you wish to Tweet. You can also add parameters to post polls, quote Tweets, Tweet with reply settings, and Tweet to Super Followers in addition to other features.
+    #payload = {"text": "Hello world! 2"}
 
-consumer_key = os.environ.get("CONSUMER_KEY")
-consumer_secret = os.environ.get("CONSUMER_SECRET")
+    # Get request token
+    request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+    oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
 
-
-# Be sure to add replace the text of the with the text you wish to Tweet. You can also add parameters to post polls, quote Tweets, Tweet with reply settings, and Tweet to Super Followers in addition to other features.
-#payload = {"text": "Hello world! 2"}
-
-# Get request token
-request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
-oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
-
-try:
-    fetch_response = oauth.fetch_request_token(request_token_url)
-except ValueError:
-    print(
-        "There may have been an issue with the consumer_key or consumer_secret you entered."
-    )
-
-resource_owner_key = fetch_response.get("oauth_token")
-resource_owner_secret = fetch_response.get("oauth_token_secret")
-print("Got OAuth token: %s" % resource_owner_key)
-
-# Get authorization
-base_authorization_url = "https://api.twitter.com/oauth/authorize"
-authorization_url = oauth.authorization_url(base_authorization_url)
-print("Please go here and authorize: %s" % authorization_url)
-verifier = input("Paste the PIN here: ")
-
-# Get the access token
-access_token_url = "https://api.twitter.com/oauth/access_token"
-oauth = OAuth1Session(
-    consumer_key,
-    client_secret=consumer_secret,
-    resource_owner_key=resource_owner_key,
-    resource_owner_secret=resource_owner_secret,
-    verifier=verifier,
-)
-oauth_tokens = oauth.fetch_access_token(access_token_url)
-
-access_token = oauth_tokens["oauth_token"]
-access_token_secret = oauth_tokens["oauth_token_secret"]
-
-# Make the request
-oauth = OAuth1Session(
-    consumer_key,
-    client_secret=consumer_secret,
-    resource_owner_key=access_token,
-    resource_owner_secret=access_token_secret,
-)
-
-
-
-while True:
-    now  = datetime.datetime.now()
-    if now.minute % 1 == 0:
-        payload = {"text": f"Forgor to set the time on my puter."}
-        # Making the request
-        response = oauth.post(
-            "https://api.twitter.com/2/tweets",
-            json=payload,
+    try:
+        fetch_response = oauth.fetch_request_token(request_token_url)
+    except ValueError:
+        print(
+            "There may have been an issue with the consumer_key or consumer_secret you entered."
         )
 
-        if response.status_code != 201:
-            raise Exception(
-                "Request returned an error: {} {}".format(response.status_code, response.text)
-            )
+    resource_owner_key = fetch_response.get("oauth_token")
+    resource_owner_secret = fetch_response.get("oauth_token_secret")
+    print("Got OAuth token: %s" % resource_owner_key)
 
-        print("Response code: {}".format(response.status_code))
-        
+    # Get authorization
+    base_authorization_url = "https://api.twitter.com/oauth/authorize"
+    authorization_url = oauth.authorization_url(base_authorization_url)
+    print("Please go here and authorize: %s" % authorization_url)
+    verifier = input("Paste the PIN here: ")
 
-        # Saving the response as JSON
-        json_response = response.json()
-        json_response_temp = json.loads(json_response)
-        json_response_temp.update({"date_time":f"{now.day} / {now.month} / {now.year} - {now.hour}.{now.minute}.{now.second}"})
-        print(json.dumps(json_response, indent=4, sort_keys=True))
-        break
+    # Get the access token
+    access_token_url = "https://api.twitter.com/oauth/access_token"
+    oauth = OAuth1Session(
+        consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=resource_owner_key,
+        resource_owner_secret=resource_owner_secret,
+        verifier=verifier,
+    )
+    oauth_tokens = oauth.fetch_access_token(access_token_url)
 
-    time.sleep(59)
-    print("damn")
+    access_token = oauth_tokens["oauth_token"]
+    access_token_secret = oauth_tokens["oauth_token_secret"]
+
+    # Make the request
+    oauth = OAuth1Session(
+        consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=access_token,
+        resource_owner_secret=access_token_secret,
+    )
+
+    return oauth
+
+#######################################################
+
+def tweetingLoop(sessAuth):
+    while True:
+        try:
+            i = random.randint(0,1516512)
+            tweetContent = f"{i}"
+            now  = datetime.datetime.now()
+
+            if now.minute % 1 == 0:
+                payload = {"text": tweetContent}
+                #Making the request, tweet smt
+                response = sessAuth.post(
+                    "https://api.twitter.com/2/tweets",
+                    json=payload,
+                )
+
+                if response.status_code != 201:
+                    raise Exception(
+                        "Request returned an error: {} {}".format(response.status_code, response.text)
+                    )
+
+                print("Response code: {}".format(response.status_code))
+                
+
+                # Saving the response as JSON
+                json_response = response.json()
+                print(json.dumps(json_response, indent=4, sort_keys=True))
+
+        except Exception:
+            print("probably shitty ass connection")
+            pass
+
+        #####################################
+        #append to history
+        try:
+            json_response["data"]["Time"] = f"{now.year}/{now.month}/{now.day} - {now.hour}.{now.minute}.{now.second}"
+
+            print("manipulated json response: \n",
+                json.dumps(json_response, indent=4, sort_keys=True))
+            
+            historyFile = "tweet_history.json"
+
+            listObj = []
+            with open(historyFile) as fp:
+                listObj = json.load(fp)
+                print("listobj: ", listObj)
+                print("listobj type: ", type(listObj))
+
+            listObj.append(json_response)
+            print("manipulated listObj: ", listObj)
+
+            with open(historyFile, "w") as json_file:
+                json.dump(listObj, json_file, indent=4)
+
+        except Exception:
+            print("appending to history didn't work.", Exception)
+        #####################################
+            
+        time.sleep(20)
+
+
+tweetingLoop(authToTwitter())
